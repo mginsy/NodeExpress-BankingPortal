5bfc3ba7ccf25fadeabf3370ec9ff4d1705fa605
mginsy <mnginsbe@usc.edu>
2023-02-17 10:09:03 -0800

    part5

--- src/app.js
+++ src/app.js
@@ -2,17 +2,13 @@ const fs = require("fs")
 const path = require("path")
 const express = require("express")
 const app = express()
+const { accounts, users, writeJSON } = require("./data")
 var PORT = 3000;
 
 app.set("views", path.join(__dirname, "views"))
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded(extended=true))
-
-const accountData = fs.readFileSync(path.join(__dirname,"json","accounts.json"), "utf8")
-const accounts = JSON.parse(accountData)
-const userData = fs.readFileSync(path.join(__dirname,"json","users.json"), "utf8")
-const users = JSON.parse(userData)
 
 app.get('/', (req, res) => {
     res.render("index",{title:"Account Summary", accounts:accounts})
@@ -44,8 +40,7 @@ app.post('/transfer', (req, res) => {
     let newBalanceTo = accounts[req.body.to].balance + parseInt(req.body.amount)
     accounts[req.body.to].balance = newBalanceTo
 
-    let accountsJSON = JSON.stringify(accounts)
-    fs.writeFileSync(path.join(__dirname,"json","accounts.json"),accountsJSON,"utf8")
+    writeJSON()
     res.render("transfer",{message:"Transfer Completed"})
 })
 
@@ -57,8 +52,7 @@ app.post('/payment', (req, res) => {
     accounts.credit.balance = parseInt(accounts.credit.balance) - parseInt(req.body.amount)
     accounts.credit.available = parseInt(req.body.amount) + parseInt(accounts.credit.available)
 
-    let accountsJSON = JSON.stringify(accounts)
-    fs.writeFileSync(path.join(__dirname,"json","accounts.json"),accountsJSON,"utf8")
+    writeJSON()
     res.render("payment",{message:"Payment Successful", account:accounts.credit})
 })
 
